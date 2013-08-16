7176e8d574e7f4ba492c43af844b9fdc6917524f
Dave Moten <davidmoten@gmail.com>
2013-08-16 14:52:51 +1000

    add PropertyReplacer class

---
+++ src/main/java/com/github/davidmoten/logan/util/PropertyReplacer.java
@@ -0,0 +1,45 @@
+package com.github.davidmoten.logan.util;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.PrintWriter;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import org.apache.commons.io.IOUtils;
+
+public class PropertyReplacer {
+
+	public static InputStream replaceSystemProperties(InputStream is) {
+		try {
+			List<String> lines = IOUtils.readLines(is);
+			ByteArrayOutputStream bytes = new ByteArrayOutputStream();
+			PrintWriter out = new PrintWriter(bytes);
+			Pattern p = Pattern.compile("\\$\\{[^\\$]*\\}");
+			boolean firstLine = true;
+			for (String line : lines) {
+				if (!firstLine)
+					out.println();
+				Matcher m = p.matcher(line);
+				while (m.find()) {
+					System.out.println("matched " + m.group());
+					String name = m.group()
+							.substring(2, m.group().length() - 1);
+					System.out.println(name);
+					String property = System.getProperty(name);
+					if (property != null)
+						line = line.replace(m.group(), property);
+				}
+				out.print(line);
+				firstLine = false;
+			}
+			out.close();
+			return new ByteArrayInputStream(bytes.toByteArray());
+		} catch (IOException e) {
+			throw new RuntimeException(e);
+		}
+	}
+}
---
+++ src/test/java/com/github/davidmoten/logan/util/PropertyReplacerTest.java
@@ -0,0 +1,37 @@
+package com.github.davidmoten.logan.util;
+
+import static org.junit.Assert.assertEquals;
+
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
+import org.apache.commons.io.IOUtils;
+import org.junit.Test;
+
+public class PropertyReplacerTest {
+
+	@Test
+	public void testReplaceWhenPropertySetToNullReplacesNothing()
+			throws IOException {
+		String text = "hello ${my.name}, how are you ${my.name}";
+		InputStream is = new ByteArrayInputStream(text.getBytes());
+		System.clearProperty("my.name");
+		String result = IOUtils.toString(PropertyReplacer
+				.replaceSystemProperties(is));
+		System.out.println(result);
+		assertEquals(text, result);
+	}
+
+	@Test
+	public void testReplaceWhenPropertySetToSomethingDoesReplacement()
+			throws IOException {
+		String text = "hello ${my.name}, how are you ${my.name}";
+		InputStream is = new ByteArrayInputStream(text.getBytes());
+		System.setProperty("my.name", "Dave");
+		String result = IOUtils.toString(PropertyReplacer
+				.replaceSystemProperties(is));
+		System.out.println(result);
+		assertEquals("hello Dave, how are you Dave", result);
+	}
+}
