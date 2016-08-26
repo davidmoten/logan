9429ad7cf90fee3245149233f319331a3f5fe9aa
Dave Moten <davidmoten@gmail.com>
2016-08-26 11:56:15 +1000

    update scripts

--- src/assembly/start.sh
+++ src/assembly/start.sh
@@ -5,4 +5,5 @@ export PORT=${1-8080}
 ## use first parameter, if missing default to 9797
 export STOP_PORT=${2-9797}
 export STOP_KEY=logan
-java -Dlogan.config=configuration.xml -jar jetty-runner.jar --port $PORT --stop-port $STOP_PORT --stop-key $STOP_KEY --out yyyy_mm_dd-logan.log logan.war
+export CONFIG=${3-configuration.xml}
+java -Dlogan.config=$CONFIG $4 $5 $6 $7 $8 $9 -jar jetty-runner.jar --port $PORT --stop-port $STOP_PORT --stop-key $STOP_KEY --out yyyy_mm_dd-logan.log logan.war
