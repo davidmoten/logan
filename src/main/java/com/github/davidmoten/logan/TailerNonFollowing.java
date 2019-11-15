6346d507239750a233e8823bcdbcbaf78c3578ee
David Moten <davidmoten@gmail.com>
2019-11-15 13:21:46 +1100

    close resources properly in TailerNonFollowing

--- src/main/java/com/github/davidmoten/logan/TailerNonFollowing.java
+++ src/main/java/com/github/davidmoten/logan/TailerNonFollowing.java
@@ -3,7 +3,6 @@ package com.github.davidmoten.logan;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
@@ -11,38 +10,37 @@ import org.apache.commons.io.input.TailerListener2;
 
 public class TailerNonFollowing implements Runnable {
 
-	private volatile boolean keepGoing = true;
-	private final File file;
-	private final TailerListener2 listener;
-	private final int bufferSize;
+    private volatile boolean keepGoing = true;
+    private final File file;
+    private final TailerListener2 listener;
+    private final int bufferSize;
 
-	public TailerNonFollowing(File file, TailerListener2 listener, int bufferSize) {
-		this.file = file;
-		this.listener = listener;
-		this.bufferSize = bufferSize;
-	}
+    public TailerNonFollowing(File file, TailerListener2 listener, int bufferSize) {
+        this.file = file;
+        this.listener = listener;
+        this.bufferSize = bufferSize;
+    }
 
-	@Override
-	public void run() {
-		FileInputStream fis;
-		try {
-			fis = new FileInputStream(file);
-			BufferedReader br = new BufferedReader(new InputStreamReader(fis),
-					bufferSize);
-			String line;
-			while (keepGoing && (line = br.readLine()) != null) {
-				listener.handle(line);
-			}
-			br.close();
-		} catch (FileNotFoundException e) {
-			listener.handle(e);
-		} catch (IOException e) {
-			listener.handle(e);
-		}
-	}
+    @Override
+    public void run() {
+        try {
+            try (BufferedReader br = //
+                    new BufferedReader( //
+                            new InputStreamReader( //
+                                    new FileInputStream(file)),
+                            bufferSize)) {
+                String line;
+                while (keepGoing && (line = br.readLine()) != null) {
+                    listener.handle(line);
+                }
+            }
+        } catch (IOException e) {
+            listener.handle(e);
+        }
+    }
 
-	public void stop() {
-		keepGoing = false;
-	}
+    public void stop() {
+        keepGoing = false;
+    }
 
 }
